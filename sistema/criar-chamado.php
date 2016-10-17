6bdb620de93484c18eaebc8d5f0ea0e13e976a42
Raphael Kieling <Raphael Kieling>
2016-10-17 10:06:17 -0200

    Implementado Prazo por opção

--- sistema/criar-chamado.php
+++ sistema/criar-chamado.php
@@ -8,16 +8,22 @@
 	$data_c        = date('d/m/Y');
 	$data_c_i      = date('d/m/Y');
 	$hora_c        = $_POST['hora_c'];
-	$prazo_c       = "2";
+	$prazo_c       = "1";
 	$n0            = $_POST['n0_c'];
 	$n1            = $_POST['n1_c'];
 	$n2            = $_POST['n2_c'];
 	$descricao_c   = $_POST['descricao_c'];
 	$status_c      = "Enviado";
-
+	
 
 	if($prazo_c <=0){
 		$prazo_c = "0";
+	}else{
+		if($problema_opcao == "Dúvida"){
+			$prazo_c = "1";
+		}else{
+			$prazo_c = "2";
+		}
 	}
 	$data_c = DateTime::createFromFormat('d/m/Y', $data_c);
 	$data_c->add(new DateInterval('P'.$prazo_c.'D')); // 2 dias
